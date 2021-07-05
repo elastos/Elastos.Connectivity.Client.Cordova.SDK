09cc77bbf1e533af03029a91741961feacc9264e
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2021-07-05 17:24:46 +0800

    Logs added

--- src/internal/connectivityhelper.ts
+++ src/internal/connectivityhelper.ts
@@ -7,15 +7,18 @@ export class ConnectivityHelper {
      * If none, user is prompted to choose one.
      */
     public static async ensureActiveConnector(onActiveConnector:()=>void, onCancelled:()=>void) {
-        if (connectivity.getActiveConnector() != null)
+        if (connectivity.getActiveConnector() != null) {
             onActiveConnector();
+        }
         else {
             // If no active connector but only one connector available, we auto-activate it.
             if (connectivity.getAvailableConnectors().length == 1) {
+                logger.log("No active connector yet but only one connector available. Setting it as default.");
                 connectivity.setActiveConnector(connectivity.getAvailableConnectors()[0].name);
                 onActiveConnector();
             }
             else {
+                logger.log("No active connector yet. Asking user to choose one.");
                 let selectedConnectorName = await connectivity.genericUIHandler.showConnectorChooser();
                 if (selectedConnectorName)
                     onActiveConnector();
